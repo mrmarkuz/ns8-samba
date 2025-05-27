884871c424f96cf515b05c33c835e8a949b5dd07
Davide Principi <davide.principi@nethesis.it>
2025-05-27 19:29:14 +0200

    feat: required min-core 3.9+
    Samba 3 will rely on the dynamic core_module flag support on the Core
    side, which will be supported starting from version 3.9.

--- build-images.sh
+++ build-images.sh
@@ -87,7 +87,7 @@ buildah add "${container}" ns8-user-manager-${user_manager_version}.tar.gz /imag
 buildah add "${container}" ui/dist /ui
 buildah config \
     --label="org.nethserver.max-per-node=1" \
-    --label="org.nethserver.min-core=3.6.3-0" \
+    --label="org.nethserver.min-core=3.9.0-0" \
     --label "org.nethserver.images=ghcr.io/nethserver/samba-dc:${IMAGETAG:-latest} docker.io/timescale/timescaledb:2.19.3-pg17" \
     --label 'org.nethserver.authorizations=node:fwadm cluster:accountprovider traefik@node:routeadm' \
     --label="org.nethserver.tcp-ports-demand=1" \
